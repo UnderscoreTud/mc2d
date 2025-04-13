fe53418b37cfed1c32a301fc087cbfb1ee0e953d
_tud <98935832+UnderscoreTud@users.noreply.github.com>
2025-04-13 15:03:53 +0300

    Add netty as a dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    implementation("io.netty:netty-all:4.2.0.Final")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
