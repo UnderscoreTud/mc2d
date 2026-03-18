78e9cc727aefe4a5f4eee9e0693caf855cab1482
_tud <98935832+UnderscoreTud@users.noreply.github.com>
2026-03-18 16:54:10 +0300

    Add ItemStack class

---
+++ server/src/main/java/me/tud/mc2d/item/ItemStack.java
@@ -0,0 +1,24 @@
+package me.tud.mc2d.item;
+
+import lombok.With;
+import me.tud.mc2d.util.FriendlyByteBuf;
+import me.tud.mc2d.util.Writable;
+
+@With
+public record ItemStack(int count, Item item) implements Writable {
+
+    public static final ItemStack AIR = new ItemStack(Items.AIR);
+
+    public ItemStack(Item item) {
+        this(1, item);
+    }
+
+    @Override
+    public void write(FriendlyByteBuf buf) {
+        buf.writeVarInt(count)
+                .writeVarInt(item.id())
+                .writeVarInt(0)  // Components to add
+                .writeVarInt(0); // Components to remove
+    }
+
+}
